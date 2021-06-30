2704ab0828c3d6f39a631a2cec37e0c7b354debf
lahiru <#L0718003955h>
2021-06-30 16:08:12 +0530

    Some new COMMIT

--- routes/EditorDashboardWorkshop.js
+++ routes/EditorDashboardWorkshop.js
@@ -2,7 +2,7 @@ const editorDashboardRouter = require('express').Router();
 let EditorDashboard = require('../models/workshopModel');
 
 editorDashboardRouter.route("/displayAllWorkshops").get((req, res)=>{
-    EditorDashboard.find({status: "un_checked"}, function(docs, err){
+    EditorDashboard.find({status: "checked"}, function(docs, err){
         if(!err){
             res.send(docs)
         }else{
